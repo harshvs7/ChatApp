9e00263c4954fc673cd4ca293cd3c14697993f89
Harshvardhan Sharma <harshvardhansharma@Harshvardhans-MacBook-Air.local>
2023-12-28 14:24:08 +0530

    First setup

---
+++ Chat Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat Application
+//
+//  Created by Harshvardhan Sharma on 28/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Chat ApplicationUITests/Chat_ApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chat_ApplicationUITestsLaunchTests.swift
+//  Chat ApplicationUITests
+//
+//  Created by Harshvardhan Sharma on 28/12/23.
+//
+
+import XCTest
+
+final class Chat_ApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
